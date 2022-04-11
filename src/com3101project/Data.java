ffbe8806840baa3fce79e56c7cd6392b9fce0a0d
KennyxMiku <94447890+KennyxMiku@users.noreply.github.com>
2022-04-11 10:55:37 +0800

    dto class

---
+++ src/com3101project/Data.java
@@ -0,0 +1,237 @@
+/**
+ * Generated on Mon Apr 11 02:34:04 UTC 2022 by ObjGen 3.0
+ */
+package com3101project;
+ 
+/**
+ * Description for MyClass
+ */
+public class Data {
+ 
+   /** Property p1Pos */
+   String p1Pos;
+ 
+   /** Property p1Balance */
+   String p1Balance;
+ 
+   /** Property p1Status */
+   String p1Status;
+ 
+   /** Property p2Pos */
+   String p2Pos;
+ 
+   /** Property p2Balance */
+   String p2Balance;
+ 
+   /** Property p2Status */
+   String p2Status;
+ 
+   /** Property p3Pos */
+   String p3Pos;
+ 
+   /** Property p3Balance */
+   String p3Balance;
+ 
+   /** Property p3Status */
+   String p3Status;
+ 
+   /** Property p4Pos */
+   String p4Pos;
+ 
+   /** Property p4Balance */
+   String p4Balance;
+ 
+   /** Property p4Status */
+   String p4Status;
+ 
+   /** Property nTurn */
+   String nTurn;
+ 
+   /**
+    * Constructor
+    */
+   public Data() {
+   }
+ 
+   /**
+    * Gets the p1Pos
+    */
+   public String getP1Pos() {
+      return this.p1Pos;
+   }
+ 
+   /**
+    * Sets the p1Pos
+    */
+   public void setP1Pos(String value) {
+      this.p1Pos = value;
+   }
+ 
+   /**
+    * Gets the p1Balance
+    */
+   public String getP1Balance() {
+      return this.p1Balance;
+   }
+ 
+   /**
+    * Sets the p1Balance
+    */
+   public void setP1Balance(String value) {
+      this.p1Balance = value;
+   }
+ 
+   /**
+    * Gets the p1Status
+    */
+   public String getP1Status() {
+      return this.p1Status;
+   }
+ 
+   /**
+    * Sets the p1Status
+    */
+   public void setP1Status(String value) {
+      this.p1Status = value;
+   }
+ 
+   /**
+    * Gets the p2Pos
+    */
+   public String getP2Pos() {
+      return this.p2Pos;
+   }
+ 
+   /**
+    * Sets the p2Pos
+    */
+   public void setP2Pos(String value) {
+      this.p2Pos = value;
+   }
+ 
+   /**
+    * Gets the p2Balance
+    */
+   public String getP2Balance() {
+      return this.p2Balance;
+   }
+ 
+   /**
+    * Sets the p2Balance
+    */
+   public void setP2Balance(String value) {
+      this.p2Balance = value;
+   }
+ 
+   /**
+    * Gets the p2Status
+    */
+   public String getP2Status() {
+      return this.p2Status;
+   }
+ 
+   /**
+    * Sets the p2Status
+    */
+   public void setP2Status(String value) {
+      this.p2Status = value;
+   }
+ 
+   /**
+    * Gets the p3Pos
+    */
+   public String getP3Pos() {
+      return this.p3Pos;
+   }
+ 
+   /**
+    * Sets the p3Pos
+    */
+   public void setP3Pos(String value) {
+      this.p3Pos = value;
+   }
+ 
+   /**
+    * Gets the p3Balance
+    */
+   public String getP3Balance() {
+      return this.p3Balance;
+   }
+ 
+   /**
+    * Sets the p3Balance
+    */
+   public void setP3Balance(String value) {
+      this.p3Balance = value;
+   }
+ 
+   /**
+    * Gets the p3Status
+    */
+   public String getP3Status() {
+      return this.p3Status;
+   }
+ 
+   /**
+    * Sets the p3Status
+    */
+   public void setP3Status(String value) {
+      this.p3Status = value;
+   }
+ 
+   /**
+    * Gets the p4Pos
+    */
+   public String getP4Pos() {
+      return this.p4Pos;
+   }
+ 
+   /**
+    * Sets the p4Pos
+    */
+   public void setP4Pos(String value) {
+      this.p4Pos = value;
+   }
+ 
+   /**
+    * Gets the p4Balance
+    */
+   public String getP4Balance() {
+      return this.p4Balance;
+   }
+ 
+   /**
+    * Sets the p4Balance
+    */
+   public void setP4Balance(String value) {
+      this.p4Balance = value;
+   }
+ 
+   /**
+    * Gets the p4Status
+    */
+   public String getP4Status() {
+      return this.p4Status;
+   }
+ 
+   /**
+    * Sets the p4Status
+    */
+   public void setP4Status(String value) {
+      this.p4Status = value;
+   }
+ 
+   /**
+    * Gets the nTurn
+    */
+   public String getNTurn() {
+      return this.nTurn;
+   }
+ 
+   /**
+    * Sets the nTurn
+    */
+   public void setNTurn(String value) {
+      this.nTurn = value;
+   }
+}
